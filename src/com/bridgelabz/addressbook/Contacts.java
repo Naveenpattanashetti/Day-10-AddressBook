0ae606ddc89e52437236349630adc2063ff8a0ec
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-10 22:20:05 +0530

    Added : Program to add a new Contact to the Address Book

--- src/com/bridgelabz/addressbook/Contacts.java
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -10,21 +10,21 @@ public class Contacts {
 	private long phoneNumber;
 	private String email;
 
-public Contacts() {
+	public Contacts() {
 
-}
+	}
 
-public Contacts(String firstName, String lastName, String address, String city, String state, int zip,
-		long phoneNumber, String email) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.address = address;
-	this.city = city;
-	this.state = state;
-	this.zip = zip;
-	this.phoneNumber = phoneNumber;
-	this.email = email;
-}
+	public Contacts(String firstName, String lastName, String address, String city, String state, int zip,
+			long phoneNumber, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
 
 	public String getFirstName() {
 		return firstName;
